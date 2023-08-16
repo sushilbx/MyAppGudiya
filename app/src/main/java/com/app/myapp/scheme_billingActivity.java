48521e02487ba6bedc23c4845d8aa4f97220c334
sushil <bxrsushil@gmail.com>
2023-08-16 08:43:20 +0530

    Initial commit

---
+++ app/src/main/java/com/app/myapp/scheme_billingActivity.java
@@ -0,0 +1,13 @@
+package com.app.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class scheme_billingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scheme_billing);
+    }
+}
